68f3703d7ad92264c65374f85c42f2e6d13f8b96
tashbetz <elhan_me@hotmail.com>
2026-09-04 14:59:24 +0300

    Delete times/hebcal-core.js

--- times/hebcal-core.js
+++
@@ -1,6 +0,0 @@
-/* esm.sh - @hebcal/core@6.9.2 */
-import "/@hebcal/hdate@^0.22.7?target=es2022";
-import "/@hebcal/noaa@^0.12.3?target=es2022";
-import "/quick-lru@^7.3.0?target=es2022";
-import "/temporal-polyfill@^1.0.3/global?target=es2022";
-export * from "/@hebcal/core@6.9.2/es2022/core.mjs";
